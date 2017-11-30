8ed274d4fcc378ca34a291aaeacba3097e48da02
zonble <zonble@gmail.com>
2017-12-01 02:28:46 +0800

    Updates the script.

--- k2y/content.js
+++ k2y/content.js
@@ -2,9 +2,10 @@
 var href = window.location.href
 var pathes = href.split("/")
 var playlist_id = pathes[pathes.length - 1]
-var fullpath = "https://k2y.herokuapp.com/playlists/" + playlist_id
+var fullpath = "https://k2y.herokuapp.com/embed_playlists/" + playlist_id
+// var fullpath = "http://localhost:5000/embed_playlists/" + playlist_id
 
-var html = "<div class='container'><a href='" + fullpath + "' style='font-size:22pt'>Play the playlist using Youtube.</a></div>";
+var html = "<iframe src='" + fullpath + "' style='width:100%; height: 370px; margin: 0; padding: 0'></iframe>";
 var wrapper = document.createElement('div');
 wrapper.innerHTML = html;
 var container = document.getElementsByClassName('container')[0]
