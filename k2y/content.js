14097bd8e66a4cc60af546b20add0742c028b726
zonble <zonble@gmail.com>
2017-12-02 23:10:24 +0800

    Adds images.

--- k2y/content.js
+++ k2y/content.js
@@ -1,9 +1,7 @@
-
 var href = window.location.href
 var pathes = href.split("/")
 var playlist_id = pathes[pathes.length - 1]
 var fullpath = "https://k2y.herokuapp.com/embed_playlists/" + playlist_id
-// var fullpath = "http://localhost:5000/embed_playlists/" + playlist_id
 
 var html = "<iframe src='" + fullpath + "' style='width:100%; height: 370px; margin: 0; padding: 0'></iframe>";
 var wrapper = document.createElement('div');
