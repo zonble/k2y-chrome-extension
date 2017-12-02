e8925f2390538e7a7cfadd2dd40f7fac0639c5e9
zonble <zonble@gmail.com>
2017-12-03 02:32:10 +0800

    Updates the safari exntension.

--- k2y_safari/k2yExtension/script.js
+++ k2y_safari/k2yExtension/script.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
+	var url = window.location.href
+	var matches = url.match(/(http|https):\/\/([a-zA-Z0-9]+).kkbox.com(\/[a-z][a-z]|)(\/[a-z][a-z]|)\/playlist\/([a-zA-Z0-9\\_\\-]+)(#[a-zA-Z0-9]+|)/)
+	if (matches.length == 0) {
+		return
+	}
+	var playlist_id = matches[5]
+
+	var fullpath = "https://k2y.herokuapp.com/embed_playlists/" + playlist_id
+
+	var html = "<iframe src='" + fullpath + "' style='width:100%; height: 370px; margin: 0; padding: 0'></iframe>";
+	var wrapper = document.createElement('div');
+	wrapper.innerHTML = html;
+	var container = document.getElementsByClassName('container')[0]
+	document.body.insertBefore(wrapper, container)
 });
